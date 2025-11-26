e560b9a33e4929eba4239fa08652454457284af9
Lokesh Reddy <lokeshreddy@Lokeshs-MacBook-Air.local>
2025-11-26 16:41:48 +0530

    Added auditing fields and BaseEntity changes

--- src/main/java/com/LokeshReddy/SpringSecuityAsymmetricEncryption/common/BaseEntity.java
+++ src/main/java/com/LokeshReddy/SpringSecuityAsymmetricEncryption/common/BaseEntity.java
@@ -6,6 +6,10 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -23,7 +27,20 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private Integer id;
 
-    @Column(name = "CREATED_BY")
-    private LocalDateTime createBy;
-    private LocalDateTime lastModifiedBy;
+    @CreatedDate
+    @Column(name = "CREATED_AT", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "UPDATED_BY")
+    private String lastModifiedBy;
+
 }
--- src/main/java/com/LokeshReddy/SpringSecuityAsymmetricEncryption/config/BeanConfig.java
+++ src/main/java/com/LokeshReddy/SpringSecuityAsymmetricEncryption/config/BeanConfig.java
@@ -2,6 +2,7 @@ package com.LokeshReddy.SpringSecuityAsymmetricEncryption.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,8 +15,13 @@ public class BeanConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    @Bean
     public AuthenticationManager authenticationManager(final AuthenticationConfiguration config)throws Exception{
         return config.getAuthenticationManager();
+    }
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return new ApplicationAuditorAware();
     }
 
 }
